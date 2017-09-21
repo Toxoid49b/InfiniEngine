90d9ca7b3bb0fa909941e451832f1969c67a5066
Toxoid49b <god@whodoyoufear.me>
2017-09-21 04:04:20 -0400

    Added Material generator and importer classes

---
+++ InfiniEngine/Generator/MaterialGenerator.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+namespace InfiniEngine.Generator {
+
+    public class MaterialGenerator {
+
+        /// <summary>
+        /// Creates a new Material from the specified paramaters
+        /// </summary>
+        public static Material CreateMaterial(string materialName, string shaderName, Texture2D materialTexture, Color materialColor, Vector2 textureOffest, Vector2 textureScale) {
+
+            Shader matShader = Shader.Find(shaderName);
+            if (matShader == null) return null;
+
+            Material newMat = new Material(matShader);
+            newMat.color = materialColor;
+            newMat.name = materialName;
+            newMat.mainTexture = materialTexture;
+            newMat.mainTextureOffset = textureOffest;
+            newMat.mainTextureScale = textureScale;
+
+            return newMat;
+
+        }
+
+        /// <summary>
+        /// Creates a new Material from the specified paramaters
+        /// </summary>
+        public static Material CreateMaterial(string materialName, string shaderName, Texture2D materialTexture, Color materialColor) {
+
+            Shader matShader = Shader.Find(shaderName);
+            if (matShader == null) return null;
+
+            Material newMat = new Material(matShader);
+            newMat.color = materialColor;
+            newMat.name = materialName;
+            newMat.mainTexture = materialTexture;
+            newMat.mainTextureOffset = new Vector2(1, 1);
+            newMat.mainTextureScale = new Vector2(1, 1);
+
+            return newMat;
+
+        }
+
+        /// <summary>
+        /// Creates a new Material from the specified paramaters
+        /// </summary>
+        public static Material CreateMaterial(string materialName, string shaderName, Texture2D materialTexture) {
+
+            Shader matShader = Shader.Find(shaderName);
+            if (matShader == null) return null;
+
+            Material newMat = new Material(matShader);
+            newMat.color = Color.white;
+            newMat.name = materialName;
+            newMat.mainTexture = materialTexture;
+            newMat.mainTextureOffset = new Vector2(1, 1);
+            newMat.mainTextureScale = new Vector2(1, 1);
+
+            return newMat;
+
+        }
+
+        /// <summary>
+        /// Creates a new Material from the specified paramaters
+        /// </summary>
+        public static Material CreateMaterial(string materialName, string shaderName, Color materialColor) {
+
+            Shader matShader = Shader.Find(shaderName);
+            if (matShader == null) return null;
+
+            Material newMat = new Material(matShader);
+            newMat.color = materialColor;
+            newMat.name = materialName;
+
+            return newMat;
+
+        }
+
+    }
+
+}
---
+++ InfiniEngine/Importers/WavefrontImporter.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace InfiniEngine.Importers {
+
+    class WavefrontImporter {
+
+        /// <summary>
+        /// Creates a mesh from a byte array containing Wavefront OBJ data
+        /// </summary>
+        public static Mesh MeshFromBytes(byte[] meshData) {
+
+            Mesh generatedMesh = new Mesh();
+            return null; // I am a talentless hack
+
+        }
+
+        /// <summary>
+        /// Creates a mesh from a file containing Wavefront OBJ data
+        /// </summary>
+        public static Mesh LoadFromFile(string filePath) {
+
+            return null; // I am a talentless hack
+
+        }
+
+        /// <summary>
+        /// Creates a mesh from a string containing Wavefront OBJ data
+        /// </summary>
+        public static Mesh LoadFromString(string objData) {
+
+            return null; // I am a talentless hack
+
+        }
+
+    }
+
+}
